403eb8c1ec0ef96635f819a8bcef2ef6df42c90d
Dev-hanseo <85448618+Dev-hanseo@users.noreply.github.com>
2021-07-08 04:36:01 +0900

    state 업데이트 함수 구현
    state 업데이트 함수 구현

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -13,19 +13,26 @@ class Habits extends Component {
 
     
     handleIncrement = (habit)=>{
-        // state 오브젝트 안에 있는 count를 증가 한뒤 state를 업데이트 해야 함.
-        // this.setState({count: this.state.count + 1});
         console.log(`handleIncrement ${habit.name}`);
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        habits[index].count++;
+        this.setState({habits});
     }
 
     handleDecrement = (habit)=>{
         console.log(`handleDecrement ${habit.name}`);
-        // const count = this.state.count - 1;
-        // this.setState({count: count < 0 ? 0 : count});
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count-1;
+        habit[index].count = count < 0 ? 0 : count;
+        this.setState({habits});
     }
 
     handleDelete = (habit)=>{
         console.log(`handleDelete ${habit.name}`);
+        const habits = this.state.habits.filter(item => item.id !== habit.id);
+        this.setState({habits});
     };
 
     render() {
