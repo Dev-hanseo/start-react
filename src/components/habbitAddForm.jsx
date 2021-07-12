0c03452ae56a350d45519373354c112f0eeae362
Dev-hanseo <85448618+Dev-hanseo@users.noreply.github.com>
2021-07-12 22:39:56 +0900

    function component
    함수형 컴포넌트

--- src/components/habbitAddForm.jsx
+++ src/components/habbitAddForm.jsx
@@ -1,29 +1,25 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-class HabbitAddForm extends PureComponent {
-    inputRef = React.createRef();
-    formRef = React.createRef();
+const HabbitAddForm = (props) => {
+    const inputRef = React.createRef();
+    const formRef = React.createRef();
 
-    onSubmit = event => {
+    const onSubmit = event => {
         event.preventDefault();
         const name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
-        // this.inputRef.current.value = "";
-        this.formRef.current.reset();
+        name && props.onAdd(name);
+        formRef.current.reset();
     }
-    render() {
-        
-        return (
-            <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-                <input 
-                ref={this.inputRef} 
-                type="text" 
-                className="add-input" 
-                placeholder="habit" />
-                <button className="add-button">add button</button>
-            </form>
-        );
-    }
+    return (
+        <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+            <input 
+            ref={inputRef} 
+            type="text" 
+            className="add-input" 
+            placeholder="habit" />
+            <button className="add-button">add button</button>
+        </form>
+    );
 }
 
 export default HabbitAddForm;
